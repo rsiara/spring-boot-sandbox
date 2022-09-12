89893215e7f49d50126be67fff4f9bbe7aa07c7b
rsiara <rsiara@empik.com>
2022-09-13 01:33:45 +0200

    minor fixes

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -27,39 +27,42 @@ public class PersonController {
 
     @GetMapping(value = "/users/{id}")
     public ResponseEntity<Person> getUser(@PathVariable("id") Integer id) {
+
         Optional<Person> user = personService.findById(id);
         return new ResponseEntity<>(user.get(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/usersByName/{name}")
     public List<Person> getUser(@PathVariable("name") String name) {
+
         List<Person> user = personService.findByName(name);
         return user;
     }
 
     @GetMapping(value = "/usersByNameList/{names}")
     public List<Person> getUser(@PathVariable("names") List<String> names) {
+
         return personService.findByNameList(names);
     }
 
     // Save operation
     @PostMapping("/users")
-    public Person saveUser(
-            @Valid @RequestBody Person person) {
+    public Person saveUser(@Valid @RequestBody Person person) {
+
         return personService.saveUser(person);
     }
 
     @PutMapping("/users/{id}")
     public Person updateUser(@RequestBody Person person,
                              @PathVariable("id") Integer userId) {
-        return personService.updateUser(
-                person, userId);
+
+        return personService.updateUser(person, userId);
     }
 
     @DeleteMapping("/users/{id}")
     public String deleteUserById(@PathVariable("id") Integer userId) {
-        personService.deleteUserById(
-                userId);
+
+        personService.deleteUserById(userId);
         return "Deleted Successfully";
     }
 }
